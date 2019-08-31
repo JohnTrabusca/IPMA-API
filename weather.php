8512624039c1caad651125ce50d53ce929788071
JohnTrabusca <40503168+JohnTrabusca@users.noreply.github.com>
2019-08-31 11:33:53 +0100

    Update weather.php

--- weather.php
+++ weather.php
@@ -817,44 +817,7 @@ $data = json_decode($jsond, TRUE);
         </div>
     </div>
 </div>
-
-<script>
-    feather.replace();
-
-    $(document).on('click','li.dias',function () {
-        var pos = $(this).val();
-
-        $('li.dias').removeClass('active');
-        $(this).addClass('active');
-
-        $('.today-info').attr('hidden',true);
-        $('#'+pos).attr('hidden',false);
-
-        $('.weather-side').attr('hidden',true);
-        $('.'+pos).attr('hidden',false);
-    });
-
-    $(document).on('click','a.locais',function () {
-        var id_local = $(this).attr('id');
-
-        var local = $(this).text();
-
-        $.ajax({
-            type: 'POST',
-            url: 'weather.php?act=locais',
-            data: {
-                id_local: id_local
-            },
-            success: function (data) {
-                $('#page_title').text('Meteorologia - '+local);
-                $('div.container').empty();
-                $('div.container').append(data);
-                feather.replace();
-            }
-        });
-
-        });
-    </script>
+<script src='assets/js/script.js'></script>
     </body>
     </html>
     <?php } ?>
